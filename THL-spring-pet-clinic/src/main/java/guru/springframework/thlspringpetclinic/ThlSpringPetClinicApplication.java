f4254916ddb58cd029f8a34342755950aeeb981e
taehalee1981 <taehalee@gmail.com>
2022-06-12 17:53:09 -0300

    Initial Project Add

---
+++ THL-spring-pet-clinic/src/main/java/guru/springframework/thlspringpetclinic/ThlSpringPetClinicApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.thlspringpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThlSpringPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThlSpringPetClinicApplication.class, args);
+    }
+
+}
---
+++ THL-spring-pet-clinic/src/test/java/guru/springframework/thlspringpetclinic/ThlSpringPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.thlspringpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThlSpringPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
