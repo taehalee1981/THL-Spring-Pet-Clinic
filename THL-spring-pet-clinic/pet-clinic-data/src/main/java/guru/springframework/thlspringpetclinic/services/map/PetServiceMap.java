14c84aef22d162c7890ac72128685a548c55c605
taehalee1981 <taehalee@gmail.com>
2022-06-15 14:04:25 -0300

    Impl Map based Services. Closes #6

---
+++ THL-spring-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/thlspringpetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,35 @@
+package guru.springframework.thlspringpetclinic.services.map;
+
+import guru.springframework.thlspringpetclinic.model.Owner;
+import guru.springframework.thlspringpetclinic.model.Pet;
+import guru.springframework.thlspringpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
