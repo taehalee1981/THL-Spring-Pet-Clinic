86571baef5bb2259e988ccfeeaf540f39dc3568a
taehalee1981 <taehalee@gmail.com>
2022-06-15 18:17:46 -0300

    List all vets on Vets index page. Closes #15

--- THL-spring-pet-clinic/pet-clinic-web/src/main/java/guru/springframework/thlspringpetclinic/bootstrap/DataLoader.java
+++ THL-spring-pet-clinic/pet-clinic-web/src/main/java/guru/springframework/thlspringpetclinic/bootstrap/DataLoader.java
@@ -40,14 +40,14 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners....");
 
         Vet vet1 = new Vet();
-        vet1.setId(3L);
+        vet1.setId(1L);
         vet1.setFirstName("Sam");
         vet1.setLastName("Axe");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(4L);
+        vet2.setId(2L);
         vet2.setFirstName("Jessie");
         vet2.setLastName("Porter");
 
--- THL-spring-pet-clinic/pet-clinic-web/src/main/java/guru/springframework/thlspringpetclinic/controllers/VetController.java
+++ THL-spring-pet-clinic/pet-clinic-web/src/main/java/guru/springframework/thlspringpetclinic/controllers/VetController.java
@@ -1,12 +1,24 @@
 package guru.springframework.thlspringpetclinic.controllers;
 
+import guru.springframework.thlspringpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
+
+    private final VetService vetService;
+
+    // Spring framework vai fazer autowired do vetService pq é um @Service bean.
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String listVets(){
+    public String listVets(Model model) {
+
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
