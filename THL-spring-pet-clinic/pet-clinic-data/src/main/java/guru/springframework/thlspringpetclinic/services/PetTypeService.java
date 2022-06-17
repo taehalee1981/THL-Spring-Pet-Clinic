e8e9ea82a831c9e785385a6b8dd43fbf78b5c66e
taehalee1981 <taehalee@gmail.com>
2022-06-17 13:21:14 -0300

    Added properties, Closes #28

---
+++ THL-spring-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/thlspringpetclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package guru.springframework.thlspringpetclinic.services;
+
+import guru.springframework.thlspringpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
