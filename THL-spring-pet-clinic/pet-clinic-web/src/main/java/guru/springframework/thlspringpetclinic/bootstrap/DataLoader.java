4e7ad1a42abc88e055249af38e1669785aab5e72
taehalee1981 <taehalee@gmail.com>
2022-06-17 13:32:53 -0300

    Added PetType to bootstrap. Closes #32

--- THL-spring-pet-clinic/pet-clinic-web/src/main/java/guru/springframework/thlspringpetclinic/bootstrap/DataLoader.java
+++ THL-spring-pet-clinic/pet-clinic-web/src/main/java/guru/springframework/thlspringpetclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package guru.springframework.thlspringpetclinic.bootstrap;
 
 import guru.springframework.thlspringpetclinic.model.Owner;
+import guru.springframework.thlspringpetclinic.model.PetType;
 import guru.springframework.thlspringpetclinic.model.Vet;
 import guru.springframework.thlspringpetclinic.services.OwnerService;
+import guru.springframework.thlspringpetclinic.services.PetTypeService;
 import guru.springframework.thlspringpetclinic.services.VetService;
 import guru.springframework.thlspringpetclinic.services.map.OwnerServiceMap;
 import guru.springframework.thlspringpetclinic.services.map.VetServiceMap;
@@ -14,15 +16,26 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
     //vai ser autowired ja que os services foram modificados para serem @Service
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        dog.setName("Cat");
+        PetType savedCatType = petTypeService.save(cat);
+
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
