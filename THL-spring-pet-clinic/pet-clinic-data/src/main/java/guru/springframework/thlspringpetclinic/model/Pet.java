9c147cd7a4f577c65820104f872103440c0508e0
taehalee1981 <taehalee@gmail.com>
2022-06-18 13:23:13 -0300

    Updated owner with JPA entity . Closes #36

--- THL-spring-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/thlspringpetclinic/model/Pet.java
+++ THL-spring-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/thlspringpetclinic/model/Pet.java
@@ -1,9 +1,24 @@
 package guru.springframework.thlspringpetclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name="pets")
 public class Pet extends BaseEntity{
+    @Column(name="name")
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name="type_id")
+    private PetType petType;
+
+    @ManyToOne
+    @JoinColumn(name="owner_id")
+    private Owner owner;
+
+    @Column(name="birth_date")
+    private LocalDate birthDate;
 
     public String getName() {
         return name;
@@ -12,10 +27,6 @@ public class Pet extends BaseEntity{
     public void setName(String name) {
         this.name = name;
     }
-
-    private PetType petType;
-    private Owner owner;
-    private LocalDate birthDate;
 
     public PetType getPetType() {
         return petType;
--- THL-spring-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/thlspringpetclinic/model/PetType.java
+++ THL-spring-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/thlspringpetclinic/model/PetType.java
@@ -1,7 +1,14 @@
 package guru.springframework.thlspringpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="types")
 public class PetType extends BaseEntity{
 
+    @Column(name="addname")
     private String name;
 
     public String getName() {
