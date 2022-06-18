7464780e8528e061e4d467e8607b8782bbafba1f
taehalee1981 <taehalee@gmail.com>
2022-06-18 15:02:14 -0300

    Implemente Map Implt for Visit Map. Closes #46

---
+++ THL-spring-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/thlspringpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package guru.springframework.thlspringpetclinic.services;
+
+import guru.springframework.thlspringpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long>{
+}
