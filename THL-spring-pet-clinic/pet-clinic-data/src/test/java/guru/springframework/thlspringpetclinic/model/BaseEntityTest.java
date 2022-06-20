c83c1879bf3bd1e9eeea093a2d8c975df0675e7c
taehalee1981 <taehalee@gmail.com>
2022-06-20 13:51:47 -0300

    Upgraded to JUnit 5. Closes #4

---
+++ THL-spring-pet-clinic/pet-clinic-data/src/test/java/guru/springframework/thlspringpetclinic/model/BaseEntityTest.java
@@ -0,0 +1,17 @@
+package guru.springframework.thlspringpetclinic.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BaseEntityTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getId() {
+    }
+}
